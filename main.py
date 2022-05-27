3b460c8d9e31e747d1176a8ca2b6e70434031dde
ndestrieux <n.destrieux@gmail.com>
2022-05-27 11:26:12 +0200

    sqlite3 queries for table creation added

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from PIL import ImageTk, Image
+import os
 import sqlite3
 
 
 root = Tk()
 root.title("My library manager")
-# icon = PhotoImage(file='/home/siiken/learning/tkinter/icons8-walter-white-50.png')
-# root.iconphoto(True, icon)
 root.geometry("600x800")
-#Set the resizable property False
+# Set the resizable property False
 root.resizable(False, False)
 # setting background image
 bg_img = Image.open('images/pexels-emre-can-acer-2079451.jpg')
@@ -21,6 +20,7 @@ canvasMain.create_image(-50, -200, image=bg_img, anchor=NW)
 canvasMain.place(x=0, y=0, relwidth=1, relheight=1)
 
 
+# open existing database
 def open_db():
     root.filename = filedialog.askopenfilename(
                                     initialdir="db/",
@@ -34,21 +34,59 @@ def open_db():
     conn.close()
 
 
+# create new database
 def create_db():
+    # create database with defined name
     def create_db_ok():
+        # check if default folder with db files does exist else it will be created
+        db_path = "db/"
+        dir_status = os.path.exists(db_path)
+        if dir_status is False:
+            os.makedirs(db_path)
+
         # Create a database
         conn = sqlite3.connect(f'db/{create_db_entry.get()}.db')
         # Create cursor
         c = conn.cursor()
+
+        # Create table album
+        c.execute("""
+                    CREATE TABLE "album" (
+                    "title"	TEXT NOT NULL,
+                    "artist_name"	TEXT NOT NULL,
+                    "year"	INTEGER,
+                    "genre"	TEXT,
+                    "format"    TEXT,
+                    "cover"	BLOB)
+                    """)
+
+        c.execute("""
+                    CREATE TABLE "books" (
+                    "title"	TEXT NOT NULL,
+                    "author"	TEXT,
+                    "year"	INTEGER,
+                    "publisher"	TEXT)
+                    """)
+
+        c.execute("""
+                    CREATE TABLE "films" (
+                    "title"	TEXT NOT NULL,
+                    "year"	INTEGER,
+                    "film_info"	TEXT,
+                    "cover"	BLOB)
+                    """)
 
         # Commit changes
         conn.commit()
 
         # Close connection
         conn.close()
-        # Create new windows to set new database name
+
+        messagebox.showinfo("Info", f"Your database {create_db_entry.get()}.db has been created successfully!")
+
         create_db_win.destroy()
 
+    # Create new windows to set new database name
     create_db_win = Toplevel(root)
     create_db_win.geometry("400x200")
     create_db_win.title("Create new database")
@@ -57,33 +95,20 @@ def create_db():
     create_db_label.grid(row=0, columnspan=2, padx=5)
     create_db_entry = Entry(create_db_win)
     create_db_entry.grid(row=1, columnspan=2, padx=5)
-    create_db_btn = Button(create_db_win, text="Create database", command=create_db_ok)
-    create_db_btn.grid(row=2, column=0, padx=5)
+    confirm_create_db_btn = Button(create_db_win, text="Create database", command=create_db_ok)
+    confirm_create_db_btn.grid(row=2, column=0, padx=5)
     create_db_exit = Button(create_db_win, text="Back", command=create_db_win.destroy)
     create_db_exit.grid(row=2, column=1, padx=5)
 
 
 open_db_btn = Button(root, text="Open database", padx=40, pady=30, command=open_db, fg="white", bg="black")
-open_db_btn.pack(pady=(50, 0))
+open_db_btn.pack(pady=(70, 0))
 
 create_db_btn = Button(root, text="Create database", padx=35, pady=30, command=create_db, fg="white", bg="black")
-create_db_btn.pack(pady=(50, 0))
+create_db_btn.pack(pady=(70, 0))
 
 exit_btn = Button(root, text="Exit", padx=40, pady=30, command=root.quit, fg="white", bg="black")
-exit_btn.pack(pady=(50, 0))
-
-'''
-# Create a database or connect to connect
-conn = sqlite3.connect('address_book.db')
-# Create cursor
-c = conn.cursor()
-
-# Commit changes
-conn.commit()
-
-# Close connection
-conn.close()
-'''
+exit_btn.pack(pady=(70, 0))
 
 
 root.mainloop()
